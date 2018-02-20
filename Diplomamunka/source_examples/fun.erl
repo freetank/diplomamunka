3104c1a36adb97750722bb940c4c531fa61a2adc
Mihály Palenik <palenik.mihaly@gmail.com>
2018-02-20 21:53:21 +0100

    Adattípusok semifinished

---
+++ Diplomamunka/source_examples/fun.erl
@@ -0,0 +1,4 @@
+1> Sum = fun (X, Y) -> X + Y end. 
+#Fun<erl_eval.12.50752066>
+2> Sum(2,3).                     
+5
---
+++ Diplomamunka/source_examples/lists.erl
@@ -0,0 +1,4 @@
+1> [vesszovel,elvalasztott,lista].
+[vesszovel,elvalasztott,lista]
+2> [fejelem | [maradek, lista]].
+[fejelem,maradek,lista]
